35869ce95628bd9abf677826f53b6bb15dd384f2
anh tien <anhtienb237@gmail.com>
2024-02-07 17:03:11 +0700

    Account adding range

---
+++ TOKENAPI/Models/DTOS/AddRangeUserDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace BackEndDevelopment.Models.DTOS
+{
+    public class AddRangeUserDTO
+    {
+        public string Email { get; set; }
+        public bool Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
